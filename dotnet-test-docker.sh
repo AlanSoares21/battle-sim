f69022d08da0eade4a73efa8901fc269a4fbab10
AlanSoares21 <alansoares26@outlook.com>
2023-02-14 21:01:29 -0300

    feat: executando testes atraves de uma imagem com o source code

--- dotnet-test-docker.sh
+++ dotnet-test-docker.sh
@@ -1,21 +1,11 @@
-VolumeName="dotnet-test-vol"
-docker volume inspect $VolumeName
-if [ $? != 0 ]; then
-    echo "Creating volume $VolumeName"
-    docker volume create $VolumeName
-fi
-Mountpoint=`docker volume inspect --format '{{ .Mountpoint }}' $VolumeName`
-echo "$VolumeName mountpoint: $Mountpoint"
-ls -l $Mountpoint
-echo "Replicating file between $PWD and $Mountpoint"
-mkdir "$Mountpoint/app"
-cp -r "$PWD/Server" "$Mountpoint/app"
-cp -r "$PWD/Engine" "$Mountpoint/app"
-cp "$PWD/dotnet-test.sh" "$Mountpoint/app/dotnet-test.sh"
-echo "Volume $VolumeName is ready."
+Dockerfile="docker/server.Dockerfile"
+DockerImage="dotnet-tests-source-code"
+echo "Creating test image $DockerImage"
+docker build -f $Dockerfile --target source-code -t $DockerImage . 
+echo "Running container with tests"
 # executando testes em um container
-docker run --rm -i -v $VolumeName:/opt mcr.microsoft.com/dotnet/sdk:7.0 << EOF
-cd /opt/app
+docker run --rm -i $DockerImage << EOF
+cd /App
 ./dotnet-test.sh
 EOF
 DotnetTestsCode=$?
@@ -26,5 +16,5 @@ else
     echo "Dotnet tests fail. Code: $DotnetTestsCode"
     Result=127
 fi
-docker volume rm $VolumeName
+docker rmi $DockerImage
 exit $Result
