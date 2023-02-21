696cb9fc480e5bf2507e8cea3aa294b362f25d0d
AlanSoares21 <alansoares26@outlook.com>
2023-02-21 19:29:14 -0300

    feat: usando volume para persistir nuget packages

--- dotnet-test-docker.sh
+++ dotnet-test-docker.sh
@@ -3,15 +3,25 @@ Dockerfile="docker/server.Dockerfile"
 DockerImage="dotnet-tests-source-code"
 echo "Creating test image $DockerImage"
 docker build -f $Dockerfile --target source-code -t $DockerImage . 
+echo "Image $Dockerfile builded."
+# creating volume to store nuget packages
+NugetPackagesVolumeName="nuget-packages-vol"
+if docker volume list | grep $NugetPackagesVolumeName; then
+    echo "Volume $NugetPackagesVolumeName is ready."
+else
+    echo "Creating volume $NugetPackagesVolumeName."
+    docker volume create $NugetPackagesVolumeName
+fi
 echo "Running container with tests"
 # executando testes em um container
-docker run --rm -i $DockerImage << EOF
+NugetPackagesPath="/root/.nuget/packages"
+docker run --rm -v $NugetPackagesVolumeName:$NugetPackagesPath -i $DockerImage << EOF
 cd /App
 ./dotnet-test.sh
 EOF
 DotnetTestsCode=$?
 Result=0
-if [ $DotnetTestsCode = 0 ]; then 
+if [ $DotnetTestsCode -eq 0 ]; then 
     echo "Succesfull tests"
 else
     echo "Dotnet tests fail. Code: $DotnetTestsCode"
