58bfa8b7efa4d88183fe1304d0b51172ca7b9ac5
AlanSoares21 <alansoares26@outlook.com>
2023-03-05 21:56:17 -0300

    feat: removendo status que não serão utilizados agora

--- Engine/Core/Interfaces/CharactersAttributes/IDefensiveAttributes.cs
+++ Engine/Core/Interfaces/CharactersAttributes/IDefensiveAttributes.cs
@@ -1,14 +1,6 @@
+namespace BattleSimulator.Engine.Interfaces.CharactersAttributes;
 
-namespace BattleSimulator.Engine.Interfaces.CharactersAttributes
+public interface IDefensiveAttributes
 {
-    public interface IDefensiveAttributes
-    {
-        double PhysicalDefenseAbsorption { get; set; }
-        double MagicalDefenseAbsorption { get; set; }
-        double Dodge { get; set; }
-        double Parry { get; set; }
-        double Block { get; set; }
-        double StealHealth { get; set; }
-        double DamageReflection { get; set; }
-    }
+    double DefenseAbsorption { get; set; }
 }
--- Engine/Core/Interfaces/CharactersAttributes/IStateAttributes.cs
+++ Engine/Core/Interfaces/CharactersAttributes/IStateAttributes.cs
@@ -2,7 +2,4 @@ namespace BattleSimulator.Engine.Interfaces.CharactersAttributes;
 
 public interface IStateAttributes {
     int Health { get; set; }
-    int HealthRegeneration { get; set; }
-    int Energy { get; set; }
-    int EnergyRegeneration { get; set; }
 }
