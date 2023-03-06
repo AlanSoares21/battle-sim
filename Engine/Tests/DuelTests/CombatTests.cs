be74dc3a1a826b1f6ece9694981c18ee91726ab9
AlanSoares21 <alansoares26@outlook.com>
2023-03-05 23:38:36 -0300

    test: falha ao atacar alvo que está a mais de um quadrado de distância

--- Engine/Tests/DuelTests/CombatTests.cs
+++ Engine/Tests/DuelTests/CombatTests.cs
@@ -23,4 +23,49 @@ public class CombatTests {
                 $"Attack failed for ({x}, {y})");
         }
     }
+
+    [TestMethod]
+    public void Attack_Fails_When_One_Square_Away_In_X_From_The_Target() 
+    {
+        IEntity attacker = Utils.FakeEntity("attacker");
+        IEntity target = Utils.FakeEntity("target");
+        IBattle battle = Utils.CreateDuelWithEntities();
+        battle.AddEntity(attacker); 
+        Coordinate targetPosition = new(4, 4);
+        battle.AddEntity(target, targetPosition);
+
+        for (int x = 2; x <= targetPosition.X + 2; x += targetPosition.X)
+        for (int y = 2; y <= targetPosition.Y + 2; y++)
+        {
+            Coordinate attackerPosition = new(x, y);
+            battle.Board.Move(attacker.Id, attackerPosition);
+            Assert.IsFalse(battle.Attack(target.Id, attacker.Id), 
+                $"Success attack from ({x}, {y})");
+        }
+    }
+
+    /*
+        Obs: in the loop of this test, four tests are repeted from the test
+        Attack_Fails_When_One_Square_Away_In_X_From_The_Target
+        (the vertices tests)
+    */
+    [TestMethod]
+    public void Attack_Fails_When_One_Square_Away_In_Y_From_The_Target() 
+    {
+        IEntity attacker = Utils.FakeEntity("attacker");
+        IEntity target = Utils.FakeEntity("target");
+        IBattle battle = Utils.CreateDuelWithEntities();
+        battle.AddEntity(attacker); 
+        Coordinate targetPosition = new(4, 4);
+        battle.AddEntity(target, targetPosition);
+
+        for (int y = 2; y <= targetPosition.Y + 2; y += targetPosition.Y)
+        for (int x = 2; x <= targetPosition.X + 2; x++)
+        {
+            Coordinate attackerPosition = new(x, y);
+            battle.Board.Move(attacker.Id, attackerPosition);
+            Assert.IsFalse(battle.Attack(target.Id, attacker.Id), 
+                $"Success attack from ({x}, {y})");
+        }
+    }
 }
