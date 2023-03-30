cd8db238acd1322824867c83922556c140c9184d
AlanSoares21 <alansoares26@outlook.com>
2023-03-29 23:49:51 -0300

    fix: rendering board grid correct

--- Clients/Web/src/pages/BattlePage/components/Board.tsx
+++ Clients/Web/src/pages/BattlePage/components/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useEffect, useRef } from "react";
+import React, { useCallback, useContext, useEffect, useMemo, useState } from "react";
 import { BattleContext } from "../BattleContext";
 import CanvasWrapper from "../../../CanvasWrapper";
 import { TBoard } from "../../../interfaces";
@@ -10,33 +10,40 @@ export interface IBoardProps { cellSize: number }
 const Board: React.FC<IBoardProps> = ({ cellSize }) => {
     const { battle } = useContext(BattleContext);
 
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [canvasRef, setCanvasRef] = useState<HTMLCanvasElement | null>(null);
+
+    const render = useMemo<BoardRenderController | undefined>(() => {
+        if (!canvasRef) {
+            console.log("canvas ref ta nulo");
+            return;
+        }
+        const context = canvasRef.getContext('2d');
+        if (!context) {
+            console.log("contexto ta nulo")
+            return;
+        }
+        const canvasWrapper = new CanvasWrapper(context)
+        const board: TBoard = battle.board;
+        const boardCanvas = new BoardCanvas(board, canvasWrapper, cellSize);
+        const value = new BoardRenderController(board, boardCanvas)
+        for (const player of battle.board.entitiesPosition) {
+            value.placePlayer({ x: player.x, y: player.y }, { name: player.entityIdentifier });
+        }
+        return value;
+    }, [ canvasRef, battle ])
 
     const handleCanvasClick = useCallback<React.MouseEventHandler<HTMLCanvasElement>>(ev => {
         
     }, []);
 
     useEffect(() => {
-        const canvas = canvasRef.current;
-        if (!canvas) {
-            console.log("canvas ta nulo");
-            return;
-        }
-        const context = canvas.getContext('2d');
-        if (!context) {
-            console.log("contexto ta nulo")
-            return;
-        }
-        const canvasWrapper = new CanvasWrapper(context)
-        const board: TBoard = { height: 6, width: 6 };
-        const boardCanvas = new BoardCanvas(board, canvasWrapper, cellSize);
-        const value = new BoardRenderController(board, boardCanvas)
-        value.render();
-    }, []);
+        if (render !== undefined)
+            render.render();
+    }, [render]);
 
     return (<div>
         <canvas 
-            ref={canvasRef} 
+            ref={setCanvasRef} 
             width={battle.board.width * cellSize} 
             height={battle.board.height * cellSize} 
             style={{border: "1px solid #f1f1f1"}}
